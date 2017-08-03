5f05de48e6e8cc98697f5bb3f0b1b511bd664788
Yamitzky <negiga@gmail.com>
2017-08-03 13:27:03 +0900

    Twitter multiple media mode

--- resolvers/twitter.py
+++ resolvers/twitter.py
@@ -16,18 +16,22 @@ def resolve(client, event):
     ids = re.findall('https?://[^/]*twitter.com/([a-zA-Z0-9_]+)(?:/status/(\d+))', event['text'])
 
     for screen_name, status_id in ids:
-        status = api.get_status(id=status_id)
-        attachment = {
+        status = api.get_status(id=status_id, tweet_mode='extended')
+        attachments = [{
             'color': '#55acee',
-            'text': status.text,
+            'text': status.full_text,
             'author_name': f'{status.user.name} @{status.user.screen_name}',
             'author_link': f'https://twitter.com/{status.user.screen_name}',
             'author_icon': f'https://twitter.com/{status.user.screen_name}/profile_image',
-        }
-        if status.entities.get('media'):
-            attachment['image_url'] = status.entities['media'][0]['media_url_https']
+        }]
+        for media in status.extended_entities.get('media', []):
+            attachments.append({
+                'color': '#55acee',
+                'title': media['media_url_https'],
+                'image_url': media['media_url_https'],
+            })
         client.api_call(
             'chat.postMessage',
             channel=event['channel'],
-            attachments=[attachment]
+            attachments=attachments
         )
