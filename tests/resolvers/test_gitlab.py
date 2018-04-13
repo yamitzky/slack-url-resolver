3f181b6d6eb45d8cb930ea74dce08cedfd6893ef
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-04-13 16:36:19 +0900

    Supports multi-line comment out

--- tests/resolvers/test_gitlab.py
+++ tests/resolvers/test_gitlab.py
@@ -3,10 +3,26 @@
 from resolvers.gitlab import trim_html_comment
 
 
-sample_message = """# Hello World.
-<!-- Test Comment -->
-This is test message.
-<!---->"""
+SAMPLE_HTML = '''# The Zen of Python
+Beautiful is better than ugly.
+<!-- comment --><!-- comment -->
+Explicit is better than implicit.
+<!--
+  Multi line comment
+-->
+Simple is better than complex.
+<!---->
+...'''
+
+EXPECTED_HTML = '''# The Zen of Python
+Beautiful is better than ugly.
+
+Explicit is better than implicit.
+
+Simple is better than complex.
+
+...'''
+
 
 def test_trim_html_comment():
-    assert trim_html_comment(sample_message) ==  '# Hello World.\n\nThis is test message.\n'
+    assert trim_html_comment(SAMPLE_HTML) ==  EXPECTED_HTML
