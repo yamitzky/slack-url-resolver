5de724113758efd62b86fc084e97ef93e2d919f6
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-04-13 16:31:29 +0900

    Let regex patterns to global.

--- resolvers/gitlab.py
+++ resolvers/gitlab.py
@@ -7,10 +7,14 @@ import requests
 from . import skip_bot_message
 
 
+GITLAB_DOMAIN = os.environ['GITLAB_DOMAIN']
+GITLAB_ID_PATTERN = re.compile(f'{GITLAB_DOMAIN}/(.+)/(issues|merge_requests)/(\d+)')
+COMMENT_PATTERN = re.compile(r'''<!--.*?-->''', re.MULTILINE | re.DOTALL)
+
+
 @skip_bot_message
 def resolve(post_message, event):
-    GITLAB_DOMAIN = os.environ['GITLAB_DOMAIN']
-    ids = re.findall(f'{GITLAB_DOMAIN}/(.+)/(issues|merge_requests)/(\d+)', event['text'])
+    ids = GITLAB_ID_PATTERN.findall(event['text'])
     for project, issue_type, issue_id in ids:
         project_id = urllib.parse.quote_plus(project)
         issue = requests.get(
@@ -30,6 +34,5 @@ def resolve(post_message, event):
         )
 
 def trim_html_comment(html):
-    COMMENT_PATTERN = re.compile(r'''<!--.*?-->''')
     return COMMENT_PATTERN.sub('', html)
     
